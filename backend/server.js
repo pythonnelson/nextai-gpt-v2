08c4a363ca85feb170ae36da1d4d1cce02b539ee
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-10 01:08:34 +0000

    Add routes to server file

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const colors = require("colors");
 const connectDb = require("./config/dbConnection");
+// routes path
+const authRoutes = require("./routes/authRoutes");
+
 
 // Call the DB connection
 connectDb();
+
 
 // Create rest object
 const app = express();
@@ -18,9 +22,12 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 
+const port = process.env.PORT || 9500;
+
+// routes
+app.use("/api/v1/auth", authRoutes);
 
 // Listening server
-const port = process.env.PORT || 9500;
 app.listen(process.env.PORT, () => {
     console.log(`Server running on ${port}`.bgCyan.white);
 });
