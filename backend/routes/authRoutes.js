166fe42926cda526ff41414b2659a0043863363b
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-10 01:09:20 +0000

    Add auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { registerController, loginController, logoutController } = require('../controllers/authController');
 
 // router object
 const router = express.Router()
 
 // routes
-router.post('/register',)
+router.post('/register', registerController);
+router.post('/login', loginController);
+router.post('/logout', logoutController);
 
 module.exports = router;
