0ea0ceca49e2be3d7d471f57aebd2378545cd31d
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-10 01:52:19 +0000

    Registration controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,38 @@
-exports.registerController = async () => {};
+const userModel = require("../models/userModel");
+const errorsResp = require("../utils/errorsResp");
+
+// JWT Token
+exports.sendToken = (user, statusCode, res) => {
+    const token = user.getSignedtoken(res);
+    res.status(statusCode).json({
+        success: true,
+        token,
+    });
+};
+
+// Registration Token
+exports.registerController = async (req,res,next) => {
+    try {
+        const { full_name, username, email, password } = req.body;
+
+        // Check if user is already registered
+        const emailExist = await userModel.findOne({ email })
+        if(emailExist) {
+            return next(new errorsResp('Email already exists.', 400))
+        }
+
+        // Register the user
+        const user = await userModel.create({
+            full_name,
+            username,
+            email,
+            password
+        })
+        sendToken(user, 201, res)
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+};
 exports.loginController = async () => {};
 exports.logoutController = async () => {};
