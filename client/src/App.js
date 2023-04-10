462822fb89f66846eeb703d18793b5d5fa1410c0
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-10 08:40:54 +0000

    Frontend structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>Next AI</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
